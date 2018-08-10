317952633b875dab938e2c3bb84b9e45e89a936c
Lee Simon <shinoblee@gmail.com>
2018-08-10 16:22:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function removeFromCart(item){
 }
 
 function placeOrder(cardNumber) {
-  if(arguments[0] == undefined){
+  if(arguments[0] === undefined){
     return "Sorry, we don\'t have a credit card on file for you.";
   }else {
     let cost = total();
